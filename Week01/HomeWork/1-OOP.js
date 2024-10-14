c2f9920be8c130439d6f19e1f875dd97081f8240
Oleksandr Gochu <oleksandr.gochu@gmail.com>
2024-10-14 23:35:45 +0100

    Creates initial structure for Week1

---
+++ Week01/HomeWork/1-OOP.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Tasks for rewriting:
+//   - Apply optimizations of computing resources: processor, memory
+//   - Minimize cognitive complexity
+//   - Respect SRP and SoC
+//   - Improve readability (understanding), reliability
+//   - Optimize for maintainability, reusability, flexibility
+//   - Make code testable
+//   - Implement simple unittests without frameworks
+//   - Try to implement in multiple paradigms: OOP, FP, procedural, mixed
